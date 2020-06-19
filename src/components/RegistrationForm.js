5b25f76fe9b603d4dcc09bc9ae0f71290f0e2362
louis baptiste <louisvis@ymail.com>
2020-06-19 15:18:14 +0000

    Automatically backed up by Learn

---
+++ src/components/RegistrationForm.js
@@ -0,0 +1,13 @@
+class RegistrationForm extends component {
+
+  render(){
+    return (
+      <form>
+      <input type="text"/>
+      <input type= "password">
+      <button type="submit"> Submit</button>
+      </form>
+    )
+
+  }
+}
